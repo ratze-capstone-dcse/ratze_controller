758a8e3ee5d0b752731dad8c292b4b8817bcc7d1
Falah Naufal Zaki <falahnzk.dev@gmail.com>
2025-10-13 20:04:51 +0700

    fix the motor direction

--- include/motor_control.h
+++ include/motor_control.h
@@ -5,9 +5,6 @@
 
 void update_pid_gain()
 {
-  // MOTOR_DRIVER_PID_KP = 0;
-  // MOTOR_DRIVER_PID_KI = 0;
-  // MOTOR_DRIVER_PID_KD = 0;
   pid_right.set_pid_gains(MOTOR_DRIVER_PID_KP, MOTOR_DRIVER_PID_KI, MOTOR_DRIVER_PID_KD);
   pid_left.set_pid_gains(MOTOR_DRIVER_PID_KP, MOTOR_DRIVER_PID_KI, MOTOR_DRIVER_PID_KD);
 }
@@ -22,8 +19,8 @@ void compute_wheel_speeds_()
   Serial.println("Wheel Speeds (rad/s): Right: " + String(v_r, 2) + " | Left: " + String(v_l, 2));
 
   // Downscale velocity for motor max velocity
-  // v_r = v_r * 0.0338;
-  // v_l = v_l * 0.0338;
+  v_r = v_r * 0.0338;
+  v_l = v_l * 0.0338;
 
   bound(v_r, -MOTOR_MAX_VELOCITY, MOTOR_MAX_VELOCITY);
   bound(v_l, -MOTOR_MAX_VELOCITY, MOTOR_MAX_VELOCITY);
@@ -58,28 +55,42 @@ inline float apply_deadband(float pwm)
 
 void motor_loop()
 {
-  compute_wheel_speeds_();
+  if (motor_update_timer_.has_elapsed())
+  {
+    compute_wheel_speeds_();
 
-  compute_motor_data();
-  auto error_motor_right = pid_right.compute(velocity_.M1);
-  auto error_motor_left = pid_left.compute(velocity_.M2);
+    compute_motor_data();
+    auto error_motor_right = pid_right.compute(velocity_.M1);
+    auto error_motor_left = pid_left.compute(velocity_.M2);
 
-  bound(error_motor_right, -255, 255);
-  bound(error_motor_left, -255, 255);
+    bound(error_motor_right, -255, 255);
+    bound(error_motor_left, -255, 255);
 
-  auto pwm_motor_right = apply_deadband(error_motor_right);
-  auto pwm_motor_left = apply_deadband(error_motor_left);
+    auto pwm_motor_right = static_cast<int>(apply_deadband(error_motor_right));
+    auto pwm_motor_left = static_cast<int>(apply_deadband(error_motor_left));
 
-  if (pwm_motor_right < 0) {
-    setTurnLeft();
+    if (pwm_motor_right > 0 && pwm_motor_left > 0)
+    {
+      moveForward(min(pwm_motor_right, pwm_motor_left));
+    }
+    else if (pwm_motor_right < 0 && pwm_motor_left > 0)
+    {
+      turnRight(abs(pwm_motor_left), abs(pwm_motor_right));
+    }
+    else if (pwm_motor_right > 0 && pwm_motor_left < 0)
+    {
+      turnLeft(abs(pwm_motor_left), abs(pwm_motor_right));
+    }
+    else if (pwm_motor_right < 0 && pwm_motor_left < 0)
+    {
+      moveBackward(min(abs(pwm_motor_right), abs(pwm_motor_left)));
+    }
+    else
+    {
+      moveStop();
+    }
+
+    Serial.println("PWM Right: " + String(pwm_motor_right) + " | PWM Left: " + String(pwm_motor_left));
+    // send_pwm(pwm_motor_left, pwm_motor_right});
   }
-  else if (pwm_motor_right > 0) {
-    setTurnRight();
-  }
-  else {
-    moveStop();
-  }
-
-  Serial.println("PWM Right: " + String(pwm_motor_right, 2) + " | PWM Left: " + String(pwm_motor_left, 2));
-  send_pwm(pwm_motor_left, pwm_motor_right);
 }
